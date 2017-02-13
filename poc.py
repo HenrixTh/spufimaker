22aca35afbbcbffe9f3be64b05d289ef5710c0aa
Arlindo Neto <neto.acs@gmail.com>
2017-02-13 00:58:24 -0200

    Add files via upload

---
+++ poc.py
@@ -0,0 +1,51 @@
+from sys import argv
+from os.path import exists
+
+# Receives Table Name as parameter
+script, sqlServerTxt = argv
+
+# valid DB2 SQL statements
+sqlStatements = {
+    'INSERT' : ['INSERT INTO ', sqlServerTxt, ' VALUES (', ');'],
+    'UPDATE' : ['UPDATE ', 'SET ', '= ', 'WHERE', ');'],
+    'DELETE' : ['DELETE FROM ', 'WHERE', ');']
+}
+
+# Creating output file (SPUFI)
+mySpufi = open('mySpufi.txt', 'w+', encoding='utf-8')
+
+# This function receives an output file as a parameter and reads input file as and object
+def sqlGen(mySpufi):
+    with open(sqlServerTxt + '.txt', encoding = 'utf-8') as queryResult:
+        # INSERT INTO sqlServerText VALUES (
+        for i in range (0, 3):
+            mySpufi.write(sqlStatements['INSERT'][i])
+
+        listOfLines = queryResult.readlines()
+
+        for i in range(2, len(listOfLines) - 3):
+            #Handling with my list of strings:
+            data = listOfLines[i].split()
+            #Writing (
+            mySpufi.write('\n    (')
+
+            for j in range(0,int((len(data) - 1)/2)):
+                mySpufi.write('\'' + data[j] + '\',')
+
+            mySpufi.write('\n    ')
+
+            for k in range(int((len(data) - 1)/2), len(data) - 1):
+                mySpufi.write('\'' + data[k] + '\',')
+
+            # Closing )
+            mySpufi.write(' \'' + data[len(data) - 1] + '\'),')
+        lastLine = listOfLines[len(listOfLines) - 1].split()
+
+        mySpufi.write('\n    (\n')
+        for j in range(1, len(lastLine) - 1):
+            mySpufi.write('    \'' + lastLine[j] + '\',\n')
+        mySpufi.write('    \'' + data[len(data) - 1] + '\'\n    )')
+    mySpufi.write('\n' + sqlStatements['INSERT'][3])
+
+sqlGen(mySpufi)
+mySpufi.close()
